660a0b6160f3a6f9d85b6796fa57fee6f3c5a2b8
virgilionkanu <virgiliocano53@gmail.com>
2022-12-09 22:21:59 -0300

    refactor: Use async await for keep playing the track

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { playAudio } from '../util';
 
 const LibrarySong = ({
   id,
@@ -10,8 +9,8 @@ const LibrarySong = ({
   isPlaying,
   setSongs,
 }) => {
-  const songSelectHandler = () => {
-    setCurrentSong(song);
+  const songSelectHandler = async () => {
+    await setCurrentSong(song);
     const newSongs = songs.map((song) => {
       if (song.id === id) {
         return {
@@ -28,7 +27,7 @@ const LibrarySong = ({
 
     setSongs(newSongs);
     //check if the song is playing
-    playAudio(isPlaying, audioRef);
+    if (isPlaying) audioRef.current.play();
   };
 
   return (
