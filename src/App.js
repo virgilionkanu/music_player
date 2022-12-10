a356195b06e9cc0a3b5ab22deb52f686a9026362
virgilionkanu <virgiliocano53@gmail.com>
2022-12-09 23:02:02 -0300

    refactor: Add mobile styles

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ const App = () => {
   };
 
   return (
-    <div className="App">
+    <div className={`App ${libraryStatus ? 'library-active' : ''}`}>
       <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
       <Song currentSong={currentSong} />
       <Player
